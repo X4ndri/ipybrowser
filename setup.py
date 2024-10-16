4d12e706c5a88b246f6edc2d7084281e96190ce8
Ahmad <ahmad.abdalqader2000@hotmail.com>
2024-10-16 17:19:33 -0400

    init commit

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ipybrowser',
+    version='0.0.1',
+    author='Ahmad Abdal Qader',
+    author_email='aqader3@gatech.edu',
+    description='A file browser for interactive python environments',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown', 
+    url='https://github.com/X4ndri/ipybrowser',  
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',  # Minimum Python version required
+    install_requires=[        # List your dependencies here
+        'numpy',              # Example dependency
+        'requests',
+        'ipython',
+        'jupyter',
+        'ipywidgets',
+        'jupyterlab-widgets'
+    ]
+)
