1abd698be12bfc2ac965d89e8e51db2b386cd6b5
Ahmad <ahmad.abdalqader2000@hotmail.com>
2024-10-16 17:22:03 -0400

    init commit

--- setup.py
+++ setup.py
@@ -15,9 +15,10 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.6',  # Minimum Python version required
-    install_requires=[        # List your dependencies here
-        'numpy',              # Example dependency
+    python_requires='>=3.6', 
+    install_requires=[
+        'numpy',
+        'pandas',
         'requests',
         'ipython',
         'jupyter',
